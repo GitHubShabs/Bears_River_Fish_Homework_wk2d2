876a72dfadca765fa1a3e42ebac847e76f341e93
Shabana Mirza <shabanamirza2010@hotmail.com>
2018-11-27 23:11:49 +0000

    Bear has empty stomach

--- bear.rb
+++ bear.rb
@@ -1,10 +1,18 @@
 class Bear
 
-  attr_reader :name, :type
+  attr_reader :name, :type, :stomach
 
-  def initialize(name, type)
+  def initialize(name, type, stomach)
     @name = name
     @type = type
+    @stomach = stomach
   end
+
+
+  def bear_has_empty_stomach
+      @stomach = [0]
+  end
+
+
 
 end
--- specs/bear_specs.rb
+++ specs/bear_specs.rb
@@ -6,13 +6,18 @@ require_relative("../bear.rb")
 class BearTest < MiniTest::Test
 
    def test_bear_has_name
-      bear = Bear.new("Yogi", "")
+      bear = Bear.new("Yogi", "", [0])
       assert_equal("Yogi", bear.name)
    end
 
    def test_bear_has_type
-      bear = Bear.new("Yogi", "Grizzly")
+      bear = Bear.new("Yogi", "Grizzly", [0])
       assert_equal("Grizzly", bear.type)
    end
 
+  def test_bear_has_empty_stomach
+      bear = Bear.new("Yogi", "Grizzly", [0])
+      assert_equal([0], bear.stomach)
   end
+
+end

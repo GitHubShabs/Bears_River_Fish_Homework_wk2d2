6967c9829d57600f0916baea67c0975f8a016eaf
Shabana Mirza <shabanamirza2010@hotmail.com>
2018-11-27 22:47:22 +0000

    bear has a name Yogi

--- specs/bear_specs.rb
+++ specs/bear_specs.rb
@@ -4,3 +4,10 @@ require_relative("../bear.rb")
 
 
 class BearTest < MiniTest::Test
+
+   def test_bear_has_name
+      bear = Bear.new("Yogi")
+      assert_equal("Yogi", bear.name)
+   end
+
+  end

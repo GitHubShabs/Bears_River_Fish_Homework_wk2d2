56fdd4e716ff8d5194d23ab6f5e56e23d034032d
Shabana Mirza <shabanamirza2010@hotmail.com>
2018-11-27 22:54:13 +0000

    bear has type - Grizzly

--- specs/bear_specs.rb
+++ specs/bear_specs.rb
@@ -6,8 +6,13 @@ require_relative("../bear.rb")
 class BearTest < MiniTest::Test
 
    def test_bear_has_name
-      bear = Bear.new("Yogi")
+      bear = Bear.new("Yogi", "")
       assert_equal("Yogi", bear.name)
+   end
+
+   def test_bear_has_type
+      bear = Bear.new("Yogi", "Grizzly")
+      assert_equal("Grizzly", bear.type)
    end
 
   end
